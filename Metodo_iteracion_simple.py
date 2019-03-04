47f1531f97d0114fb404129bcd11f1a2a70039c7
Marco Velazquez <antoniovz377@gmail.com>
2019-03-03 22:47:42 -0800

    bugs pendientes

---
+++ Metodo_iteracion_simple.py
@@ -0,0 +1,83 @@
+#Método iteración simple de punto fijo
+import math as mth
+from py_expression_eval import Parser
+
+Parser = Parser()
+
+def Error(xi,gxi,iteracion):
+
+    if iteracion == 1:
+
+        error = 0
+    else:
+
+        error = float(mth.fabs((gxi - xi)/gxi)*100)
+
+    return error
+
+def Gxi(xi,gx):
+    e = mth.e
+    x = xi
+
+    gxi = float(eval(gx))
+
+    return gxi
+
+def Fxi(xi,fx):
+
+    e = mth.e
+    x = xi
+
+    fxi = float(eval(fx))
+
+    return fxi
+def Iteracion_simple(xi,gx,fx,error_parada,iteracion):
+
+    iteracion += 1
+
+    if iteracion != 1:
+
+        if Error(xi,Gxi(xi,gx),iteracion) > error_parada:
+
+            print(iteracion,"\t\t",int(xi),"\t",int(Gxi(xi,gx)),"\t",int(Fxi(xi,fx)),"\t",int(Error(xi,Gxi(xi,gx),iteracion)))
+
+            Iteracion_simple(Gxi(xi,gx),gx,fx,error_parada,iteracion)
+        else:
+            print("""
+            La tolerancia ha sido satisfacida en g(x) igual a """,Gxi(xi,gx)," y un error de ",Error(xi,Gxi(xi,gx),iteracion),"%\n")
+            return False
+
+    else:
+
+        if Error(xi,Gxi(xi,gx),iteracion) != error_parada:
+
+            print(iteracion,"\t\t",int(xi),"\t",int(Gxi(xi,gx)),"\t",int(Fxi(xi,fx)),"\t",int(Error(xi,Gxi(xi,gx),iteracion)))
+
+            Iteracion_simple(Gxi(xi,gx),gx,fx,error_parada,iteracion)
+        else:
+            print("""
+            La tolerancia ha sido satisfacida en g(x) igual a """,Gxi(xi,gx)," y un error de ",Error(xi,Gxi(xi,gx),iteracion),"%\n")
+            return False
+
+
+
+def Menu():
+    #Encabezado
+    print("\n\t MÉTODOS NUMERICOS\n\n\tMétodo de punto fijo\n\n")
+
+    fx = input("Ingresa f(x) ---> ")
+    gx = input("\nIngresa g(x) ---> ")
+
+    xi = float(input("\nIngresa el valor de xi ---> "))
+
+    error_parada = float(input("Introduce el error de parada ---> "))
+
+    print("Iteracion\txi\tg(xi)\tf(xi)\tError")
+
+    Iteracion_simple(xi,gx,fx,error_parada,0)
+
+Menu()
+        
+
+
+        
